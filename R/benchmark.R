c8e3e58b3ad7191e5abe99728c9b43f7ad173baf
Christian Panse <cp@fgcz.ethz.ch>
2024-10-31 18:12:49 +0100

    feat: add benchmark function

---
+++ R/benchmark.R
@@ -0,0 +1,21 @@
+#R
+
+#' f <- "/Users/cp/Library/Caches/org.R-project.R/R/ExperimentHub/46314c3933e2_4590.raw"
+.benchmark <- function(f){
+   stopifnot(file.exists(f))
+
+   rawrr::readFileHeader(f)$`Number of scans` -> n 
+
+   2**(seq(0, floor(log(n, 2)))) |>
+            lapply(FUN = function(i){
+                sample(n, size = i) |> sort() -> idx
+                message("Reading ", i, "random scans from ", f)
+		start.time <- Sys.time()
+		rawrr::readSpectrum(f, scan = idx) -> S
+		end.time <- Sys.time()
+                message("in ", end.time - start.time)
+		data.frame(count = i,
+                    size = object.size(S) |> as.integer(),
+                    runTimeInSec = as.double(difftime(end.time, start.time, units='secs')))
+	}) |> Reduce(f = rbind)
+}
