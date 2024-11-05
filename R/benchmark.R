4e62886930283318c13aea41c779b3b3fadd761e
Christian Panse <cp@fgcz.ethz.ch>
2024-11-05 22:45:13 +0100

    doc: fix benchmark examples

--- R/benchmark.R
+++ R/benchmark.R
@@ -1,25 +1,31 @@
 #R
 
 #' Benchmark spectra per second
+#'
+#' @inheritParams readFileHeader
 #' @importFrom utils object.size
 #' @examples
 #' sampleFilePath() |> rawrr:::.benchmark() -> S
-#' plot (count / S$runTimeInSec ~ count, log='xy', data=S,
+#'
+#' plot (count / runTimeInSec ~ count,
+#'   log='xy',
+#'   data=S,
 #'   sub = paste0("Overall runtime took ", round(sum(S$runTimeInSec), 3), " seconds."),
-#'   xlab = 'number of random generated scan ids')
+#'   xlab = 'number of random generated scan ids',
 #'   main = "benchmark spectra per second")
+#'
 #' @author Christian Panse 2024-11-05
-.benchmark <- function(f){
-   stopifnot(file.exists(f))
+.benchmark <- function(rawfile){
+   stopifnot(file.exists(rawfile))
 
-   rawrr::readFileHeader(f)$`Number of scans` -> n 
+   readFileHeader(rawfile)$`Number of scans` -> n 
 
    2**(seq(0, floor(log(n, 2)))) |>
             lapply(FUN = function(i){
                 sample(n, size = i) |> sort() -> idx
-                message("Reading ", i, " random scans from ", f, " ...")
+                message("Reading ", i, " random scans from ", rawfile, " ...")
 		start.time <- Sys.time()
-		rawrr::readSpectrum(f, scan = idx) -> S
+		readSpectrum(rawfile, scan = idx) -> S
 		end.time <- Sys.time()
                 message("in ", end.time - start.time)
 		data.frame(count = i,
