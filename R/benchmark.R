2c2bf2559e3a557fbd8a999313351bc346afde01
Christian Panse <cp@fgcz.ethz.ch>
2024-11-05 22:03:44 +0100

    doc: benchmark cosmetics

--- R/benchmark.R
+++ R/benchmark.R
@@ -1,7 +1,14 @@
 #R
 
+#' Benchmark spectra per second
 #' @importFrom utils object.size
-# f <- "/Users/cp/Library/Caches/org.R-project.R/R/ExperimentHub/46314c3933e2_4590.raw"
+#' @examples
+#' sampleFilePath() |> rawrr:::.benchmark() -> S
+#' plot (count / S$runTimeInSec ~ count, log='xy', data=S,
+#'   sub = paste0("Overall runtime took ", round(sum(S$runTimeInSec), 3), " seconds."),
+#'   xlab = 'number of random generated scan ids')
+#'   main = "benchmark spectra per second")
+#' @author Christian Panse 2024-11-05
 .benchmark <- function(f){
    stopifnot(file.exists(f))
 
@@ -10,7 +17,7 @@
    2**(seq(0, floor(log(n, 2)))) |>
             lapply(FUN = function(i){
                 sample(n, size = i) |> sort() -> idx
-                message("Reading ", i, "random scans from ", f)
+                message("Reading ", i, " random scans from ", f, " ...")
 		start.time <- Sys.time()
 		rawrr::readSpectrum(f, scan = idx) -> S
 		end.time <- Sys.time()
